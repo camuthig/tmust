47b20ba8c72b50ece87e515676f57305af45e687
Chris Muthig <camuthig@gmail.com>
2019-05-23 22:01:14 +0930

    Read project config file and start session with basic settings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate clap;
 extern crate dirs;
 extern crate handlebars;
-#[macro_use]
+extern crate run_script;
+extern crate serde;
 extern crate serde_json;
+extern crate serde_yaml;
 use clap::{App, SubCommand, ArgMatches, Arg};
+use handlebars::Handlebars;
+use run_script::ScriptOptions;
+use serde_json::json;
+use serde::{Serialize, Deserialize};
+use std::env;
 use std::fs;
 use std::fs::File;
-use handlebars::Handlebars;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
@@ -25,7 +31,11 @@ fn main() {
                          .index(1)
                          .required(true)))
         .subcommand(SubCommand::with_name("start")
-                    .about("Start a new new tmux session"))
+                    .about("Start a new new tmux session")
+                    .arg(Arg::with_name("project")
+                         .help("The name of the new tmux project")
+                         .index(1)
+                         .required(true)))
         .get_matches();
 
     match matches.subcommand() {
@@ -35,10 +45,21 @@ fn main() {
     }
 }
 
-fn tmux() -> ExitStatus {
-    let mut command = Command::new("tmux");
+fn tmux(config: Config) -> i32 {
+    let mut reg = Handlebars::new();
 
-    command.status().expect("Unable to start the tmux session")
+    let template_str = fs::read_to_string("start.sh.hbs").unwrap();
+    let script = reg.render_template(&template_str, &json!(config)).unwrap();
+
+    println!("{:?}", script);
+
+    let mut options = ScriptOptions::new();
+    options.capture_output = false;
+    let args = vec![];
+
+    let (code, _, _) = run_script::run(&script, &args, &options).unwrap();
+
+    code
 }
 
 fn config_path() -> PathBuf {
@@ -57,6 +78,41 @@ fn config_path() -> PathBuf {
     }
 
     config_dir
+}
+
+fn config_file(project: &str) -> File {
+    let mut config_dir = config_path();
+
+    config_dir.push(project.to_owned() + ".yaml");
+
+    if !config_dir.as_path().exists() {
+        panic!("Config file does not exist for project. Try use \"start\" instead");
+    }
+
+    let f = File::open(config_dir.as_path()).expect("Unable to read configuration file");
+
+    f
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    #[serde(default = "get_shell")]
+    shell: String,
+    name: String,
+    root: String,
+}
+
+fn get_shell() -> String {
+    env::var("SHELL").unwrap()
+}
+
+fn get_config(project: &str) -> Config {
+    let config_file = config_file(project);
+
+    let config: Config = serde_yaml::from_reader(config_file).expect("Unable to parse config file");
+
+    config
 }
 
 
@@ -80,12 +136,17 @@ fn new(matches: &ArgMatches) {
 
     let mut f = File::create(config_path.as_path()).expect("Unable to create new project file");
 
-    f.write(config_content.as_bytes());
+    f.write(config_content.as_bytes()).expect("Unable to write configuration template");
 }
 
 fn start(matches: &ArgMatches) {
     // TODO Find the configuration file from the configuration directory
+    let project = matches.value_of("project").unwrap();
+    let config = get_config(project);
+
     // TODO parse configuration file
+
     // TODO start session using configuration values
-    tmux();
+    println!("Starting {}...", project);
+    tmux(config);
 }
