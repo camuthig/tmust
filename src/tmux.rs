d969e6eb8126adb4fb827ab640e516b34551bf73
Chris Muthig <camuthig@gmail.com>
2019-07-06 08:45:18 -0700

    Create a Project struct and support custom tmux command

--- src/tmux.rs
+++ src/tmux.rs
@@ -10,13 +10,14 @@ use serde::{Serialize, Deserialize};
 use serde_json::json;
 use std::env;
 use std::process::Command;
+use std::str::FromStr;
 
 fn get_shell() -> String {
     env::var("SHELL").unwrap()
 }
 
 fn get_tmux() -> String {
-    String::from_str("tmux")
+    String::from_str("tmux").unwrap()
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -53,56 +54,67 @@ pub struct Window {
 //    commands: Vec<String>,
 //}
 
-pub fn start(config: Config) -> i32 {
-    let reg = handlebars_registry();
-
-    let template_str = include_str!("start.sh.hbs");
-    let script = reg.render_template(&template_str, &json!(config)).unwrap();
-
-    let mut options = ScriptOptions::new();
-    options.capture_output = false;
-
-    let args = vec![];
-
-    let (code, _, _) = run_script::run(&script, &args, &options).unwrap();
-
-    code
+pub struct Project {
+    config: Config,
 }
 
-pub fn stop(config: Config) -> i32 {
-    let reg = handlebars_registry();
+impl Project {
+	pub fn new(config: Config) -> Project {
+		Project { config }
+	}
 
-    let template_str = include_str!("stop.sh.hbs");
-    let script = reg.render_template(&template_str, &json!(config)).unwrap();
+    pub fn start(&self) -> i32 {
+        let reg = handlebars_registry();
 
-    let mut options = ScriptOptions::new();
-    options.capture_output = false;
+        let template_str = include_str!("start.sh.hbs");
+        let script = reg.render_template(&template_str, &json!(self.config)).unwrap();
 
-    let args = vec![];
+        let mut options = ScriptOptions::new();
+        options.capture_output = false;
 
-    let (code, _, _) = run_script::run(&script, &args, &options).unwrap();
+        let args = vec![];
 
-    code
+        let (code, _, _) = run_script::run(&script, &args, &options).unwrap();
+
+        code
+    }
+
+    pub fn stop(&self) -> i32 {
+        let reg = handlebars_registry();
+
+        let template_str = include_str!("stop.sh.hbs");
+        let script = reg.render_template(&template_str, &json!(self.config)).unwrap();
+
+        let mut options = ScriptOptions::new();
+        options.capture_output = false;
+
+        let args = vec![];
+
+        let (code, _, _) = run_script::run(&script, &args, &options).unwrap();
+
+        code
+    }
+
 }
 
-pub fn has_session(project: &String) -> bool {
-    // TODO Handle the custom command here...
-    let output = Command::new("tmux")
-        .arg("list-sessions")
-        .output()
-        .unwrap();
+fn session_exists(tmux_command: &String, name: &String) -> bool {
+	let output = Command::new(tmux_command)
+		.arg("list-sessions")
+		.output()
+		.unwrap();
 
-    let r = Regex::new(&format!("^{}:", project).to_string()).unwrap();
+	let r = Regex::new(&format!("^{}:", name).to_string()).unwrap();
 
-    r.is_match(&String::from_utf8(output.stdout).unwrap())
+	r.is_match(&String::from_utf8(output.stdout).unwrap())
 }
 
-pub fn handlebars_registry() -> Handlebars {
-    let mut h = Handlebars::new();
 
-    h.register_helper("has_session", Box::new(has_session_helper));
+fn handlebars_registry() -> Handlebars {
+	let mut h = Handlebars::new();
 
-    return h;
+	h.register_helper("session_exists", Box::new(session_exists_helper));
+
+	return h;
 }
 
-handlebars_helper!(has_session_helper: |name: str| has_session(&name.to_string()));
+handlebars_helper!(session_exists_helper: |tc: str, n: str| session_exists(&tc.to_string(), &n.to_string()));
